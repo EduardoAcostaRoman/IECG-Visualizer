081bced2128a9a897d72dc80ed8e6b9d4c62b512
Eduardo Acosta <lalo_ar03@hotmail.com>
2023-07-08 19:17:03 -0700

    Add frontend .babelrc file

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const PORT = 3001;
 
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/soccerDB', {
+mongoose.connect('mongodb://localhost/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
