931bffb1067603e8380f197a109d915e26a93651
Eduardo Acosta <lalo_ar03@hotmail.com>
2023-07-08 18:47:13 -0700

    Initializing repo

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import routes from './routes/soccerRoutes';
+import cors from 'cors';
+
+// node express local server
+const app = express();
+const PORT = 3001;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/soccerDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
