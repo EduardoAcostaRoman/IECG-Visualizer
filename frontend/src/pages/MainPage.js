67e5a23ad6b3a27c439d3e0796d735b2f0dbc4bb
Eduardo Acosta <lalo_ar03@hotmail.com>
2024-01-09 22:28:26 -0700

    Delete unnecessary files

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,13 +1,10 @@
-import Layout from '@/components/layout/Layout';
 import MainNavigation from '@/components/layout/MainNavigation';
 
 function MainPage() {
   return (
     <div>
       <MainNavigation />
-      <Layout>
         <h1>IECG Visualizer</h1>
-      </Layout>
     </div>
   );
 }
